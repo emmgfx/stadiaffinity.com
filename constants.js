3449c38744624894f557d70481262a1e1dd58856
Josep Viciana <emmgfx@gmail.com>
2022-02-10 21:45:06 +0100

    Log

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 // if Vercel prod deployment, then use main domain; else use temporary domain unless you're on dev
-export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN
-  ? process.env.NEXT_PUBLIC_DOMAIN
+export const DOMAIN = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? process.env.NEXT_PUBLIC_VERCEL_URL
   : process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO;
