9929bb6dfd9bb4025b7b58cb8621b1bd0ce8747d
Josep Viciana <emmgfx@gmail.com>
2022-02-25 20:40:13 +0100

    Install SVGR

--- next.config.js
+++ next.config.js
@@ -7,4 +7,14 @@ module.exports = {
       "googleusercontent.com",
     ],
   },
+  webpack(config) {
+    // Add SVGR:
+    // https://react-svgr.com/docs/next/
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
