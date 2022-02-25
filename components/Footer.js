1ed7ae71962440ade32574063f585d2197e1a886
Josep Viciana <emmgfx@gmail.com>
2022-02-25 20:41:18 +0100

    Footer images as SVG

--- components/Footer.js
+++ components/Footer.js
@@ -2,14 +2,18 @@ import Link from "next/link";
 import Container from "./Container";
 import Divider from "./Divider";
 
+import Isotype from "../public/images/isotype.svg";
+import Logotype from "../public/images/logotype.svg";
+import BuyMeACoffee from "../public/images/buymeacoffee.svg";
+
 const Footer = () => {
   return (
     <footer className="py-8 bg-black">
       <Container className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-[5%]">
         <div className="font-light text-center md:text-left">
           <div className="flex gap-4 items-center mb-4 text-sm justify-center md:justify-start">
-            <img src="/images/isotype.svg" width="53" height="34" />
-            <img src="/images/logotype.svg" width="126" height="23" />
+            <Isotype />
+            <Logotype />
           </div>
           <p className="mb-4">
             Stadia is a copyright from Google. All images and trademarks are
@@ -21,12 +25,11 @@ const Footer = () => {
             Do you want to <strong>support our work</strong>?
           </h3>
 
-          <a
-            href="https://www.buymeacoffee.com/emmgfx"
-            className="inline-block mx-auto"
-          >
-            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=emmgfx&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" />
-          </a>
+          <Link href="https://www.buymeacoffee.com/emmgfx">
+            <a target="_blank" className="inline-block mx-auto">
+              <BuyMeACoffee />
+            </a>
+          </Link>
         </div>
         <div className="text-center md:text-right">
           <h3 className="mb-4">
