8d7cf2792139270dbc086223ec0070c2e6bde2fc
Josep Viciana <emmgfx@gmail.com>
2022-03-03 21:19:40 +0100

    IndeterminateProgressBar bug

--- components/IndeterminateProgressBar.js
+++ components/IndeterminateProgressBar.js
@@ -1,6 +1,6 @@
 const IndeterminateProgressBar = () => {
   return (
-    <div className="absolute z-20 w-full h-0.5 bg-secondary-950 overflow-hidden">
+    <div className="fixed z-20 w-full h-0.5 bg-secondary-950 overflow-hidden">
       <div className="w-full h-full bg-secondary-500 animate-[indeterminate_1s_linear_infinite] origin-[0%_50%]"></div>
     </div>
   );
