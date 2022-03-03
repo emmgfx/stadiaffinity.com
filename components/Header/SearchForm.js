a305efe204c7b0bae19756f370fdb3012e570544
Josep Viciana <emmgfx@gmail.com>
2022-03-03 21:17:16 +0100

    Simplify search behaviour

--- components/Header/SearchForm.js
+++ components/Header/SearchForm.js
@@ -1,48 +1,23 @@
-import { useEffect, useState } from "react";
-import { useDebounce } from "react-use";
-import Router, { useRouter } from "next/router";
+import { useSelector, useDispatch } from "react-redux";
+import { useRouter } from "next/router";
 
-// ? Why I'm using Router.push?
-// https://github.com/vercel/next.js/issues/18127#issuecomment-988959843
+import { setTerm } from "../../store/slices/search";
 
 const SearchForm = () => {
   const router = useRouter();
-  const [term, setTerm] = useState("");
-  const [debouncedTerm, setDebouncedTerm] = useState(null);
-
-  const [, cancel] = useDebounce(() => setDebouncedTerm(term), 1000, [term]);
-
-  useEffect(() => {
-    if (!router.query.term) return;
-    setTerm(router.query.term || "");
-  }, [router.query]);
-
-  useEffect(() => {
-    if (debouncedTerm === null) return; // For refresh
-    if (debouncedTerm !== "") {
-      Router.push({
-        pathname: "/search/[term]",
-        query: { term: debouncedTerm },
-      });
-    } else if (router.route === "/search/[term]") {
-      Router.push("/");
-    }
-  }, [debouncedTerm, router.route]);
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      if (!url.startsWith(`/search/`)) setTerm("");
-    };
-    router.events.on("routeChangeStart", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChange);
-    };
-  }, [setTerm, router.events]);
+  const dispatch = useDispatch();
+  const { term } = useSelector((state) => state.search);
 
   const submit = (e) => {
     e.preventDefault();
-    cancel();
-    setDebouncedTerm(term);
+    if (term !== "") {
+      router.push({
+        pathname: "/search/[term]",
+        query: { term },
+      });
+    } else if (router.route === "/search/[term]") {
+      router.push("/");
+    }
   };
 
   return (
@@ -55,7 +30,7 @@ const SearchForm = () => {
         value={term}
         className="w-full py-3 px-4 pl-11 text-center rounded-full bg-white/10 focus:bg-white/20 backdrop-blur-lg font-extralight focus:outline-none placeholder:text-white"
         placeholder="Search games and rate them"
-        onChange={(e) => setTerm(e.target.value)}
+        onChange={(e) => dispatch(setTerm(e.target.value))}
       />
     </form>
   );
