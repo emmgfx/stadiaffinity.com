d4167f5a4e703d67bab6164456bcc5e277816b3c
Josep Viciana <emmgfx@gmail.com>
2022-03-07 21:58:40 +0100

    Test with autoRefreshToken

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -6,4 +6,6 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 // ToDo: Check this for refresh token and persist session:
 // https://supabase.com/docs/reference/javascript/initializing#with-additional-parameters
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  autoRefreshToken: true,
+});
