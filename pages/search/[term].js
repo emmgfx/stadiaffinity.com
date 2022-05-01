b7abd0b314c13cc92008fda22088c653b944d185
Josep Viciana <emmgfx@gmail.com>
2022-05-01 21:21:46 +0200

    Improve search behaviour

--- pages/search/[term].js
+++ pages/search/[term].js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import { supabase } from "../../utils/supabaseClient";
+
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import GamesGrid from "../../components/GamesGrid";
@@ -11,15 +12,19 @@ import TextGradient from "../../components/TextGradient";
 const Search = () => {
   const router = useRouter();
   const [results, setResults] = useState([]);
+  const [searching, setSearching] = useState(false);
 
   useEffect(() => {
     if (!router.query.term) return;
+    setSearching(true);
+    setResults([]);
     const search = async (term) => {
       const { data, error } = await supabase.rpc("get_search", {
         term_input: term,
       });
       if (data) setResults(data);
       if (error) console.error(error);
+      setSearching(false);
     };
     search(router.query.term);
   }, [router.query.term]);
@@ -29,10 +34,24 @@ const Search = () => {
       <Header />
       <main>
         <Container>
-          <h1 className="text-4xl my-10">
-            <TextGradient>{results.length} results</TextGradient> for{" "}
-            <strong>{router.query.term}</strong>
-          </h1>
+          {searching && (
+            <h1 className="text-4xl my-10">
+              <TextGradient>Searching</TextGradient> for{" "}
+              <strong>{router.query.term}</strong>
+            </h1>
+          )}
+          {!searching && results.length === 0 && (
+            <h1 className="text-4xl my-10">
+              <TextGradient>Nothing found</TextGradient> for{" "}
+              <strong>{router.query.term}</strong>
+            </h1>
+          )}
+          {!searching && results.length > 0 && (
+            <h1 className="text-4xl my-10">
+              <TextGradient>{results.length} results</TextGradient> for{" "}
+              <strong>{router.query.term}</strong>
+            </h1>
+          )}
           <GamesGrid games={results} />
           <div className="h-40" />
         </Container>
