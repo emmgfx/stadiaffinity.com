23069e96eee86b9a1b4a53e0dfbc7e49f240fdb9
Josep Viciana <emmgfx@gmail.com>
2022-05-15 23:15:23 +0200

    Base reset-password page

---
+++ pages/reset-password.js
@@ -0,0 +1,20 @@
+import Header from "../components/Header";
+import Container from "../components/Container";
+import Footer from "../components/Footer";
+
+const ResetPassword = ({}) => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1 className="text-4xl my-10 font-semibold">Reset password</h1>
+          <p>ToDo</p>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default ResetPassword;
