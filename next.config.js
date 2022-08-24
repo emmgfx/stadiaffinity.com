925b6901f9a5fbf9681f4b917ee0f5a0c9dfdc39
Josep Viciana <emmgfx@gmail.com>
2022-08-24 20:51:18 +0200

    Install next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: [
@@ -32,4 +36,4 @@ module.exports = {
 
     return config;
   },
-};
+});
