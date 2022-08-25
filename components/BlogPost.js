623067170bb2b151199805bfb2295400a43c00e1
Josep Viciana <emmgfx@gmail.com>
2022-08-25 22:18:45 +0200

    Blog post small tweaks

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,25 +1,26 @@
 import Link from "next/link";
-import Image from "next/future/image";
 
 const BlogPost = ({ post }) => {
   return (
-    <article className="flex flex-col">
-      <img
-        src={post.jetpack_featured_media_url}
-        className="w-full aspect-video object-cover"
-        alt=""
-      />
-      <div className="p-6 bg-white/10 flex-grow">
-        <div className="font-normal text-sm mb-2">
-          {new Date(post.date).toLocaleDateString()}
-        </div>
-        <h1 className="line-clamp-3 font-bold text-xl">
-          <Link href={post.link}>
-            <a target="_blank">{post.title.rendered}</a>
-          </Link>
-        </h1>
-      </div>
-    </article>
+    <Link href={post.link}>
+      <a target="_blank" className="flex flex-col">
+        <article className="flex flex-col grow transition hover:bg-white/10">
+          <img
+            src={post.jetpack_featured_media_url}
+            className="w-full aspect-video object-cover"
+            alt=""
+          />
+          <div className="p-6 bg-white/10 flex-grow">
+            <div className="font-normal text-sm mb-2">
+              {new Date(post.date).toLocaleDateString()}
+            </div>
+            <h1 className="line-clamp-3 font-bold text-xl">
+              {post.title.rendered}
+            </h1>
+          </div>
+        </article>
+      </a>
+    </Link>
   );
 };
 
