c71ec3f07be1b1ba272967b8091351ea4c43957e
Josep Viciana <emmgfx@gmail.com>
2022-08-27 23:13:32 +0200

    Wrap entire game item inside an a

--- components/GameItem.js
+++ components/GameItem.js
@@ -7,36 +7,34 @@ import AffinityBar from "./AffinityBar";
 
 const GameItem = ({ game, showAffinity = false }) => {
   return (
-    <article className="rounded overflow-hidden flex flex-col">
-      <Link href={`/game/${encodeId(game.id)}`}>
-        <a>
+    <Link href={`/game/${encodeId(game.id)}`}>
+      <a className="flex flex-col">
+        <article className="rounded overflow-hidden flex flex-col h-full">
           <Cover game={game} />
-        </a>
-      </Link>
-      <div className="px-4 py-6 bg-gradient-to-r from-gray-medium to-[#363740] flex-auto flex flex-col">
-        <h3 className="font-medium mb-2 line-clamp-2">
-          <Link href={`/game/${encodeId(game.id)}`}>{game.name}</Link>
-        </h3>
-        {showAffinity && (
-          <>
-            <div className="grow" />
-            <div className="font-bold">
-              <AffinityBar gameId={game.id} />
-            </div>
-          </>
-        )}
-        {"average" in game && (
-          <div className="flex items-center gap-0.5">
-            <Stars amount={game.average} />
-            {"counter" in game && (
-              <span className="ml-2 text-[#95949B] text-sm">
-                ({game.counter})
-              </span>
+          <div className="px-4 py-6 flex-auto flex flex-col bg-gradient-to-r from-gray-medium to-[#363740]">
+            <h3 className="font-medium mb-2 line-clamp-2">{game.name}</h3>
+            {showAffinity && (
+              <>
+                <div className="grow" />
+                <div className="font-bold">
+                  <AffinityBar gameId={game.id} />
+                </div>
+              </>
+            )}
+            {"average" in game && (
+              <div className="flex items-center gap-0.5">
+                <Stars amount={game.average} />
+                {"counter" in game && (
+                  <span className="ml-2 text-[#95949B] text-sm">
+                    ({game.counter})
+                  </span>
+                )}
+              </div>
             )}
           </div>
-        )}
-      </div>
-    </article>
+        </article>
+      </a>
+    </Link>
   );
 };
 
