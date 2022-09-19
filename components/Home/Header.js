248ad1aeb6b0efac071519a0d06bf3676a7f7122
Josep Viciana <emmgfx@gmail.com>
2022-09-19 09:40:26 +0200

    100 games link

--- components/Home/Header.js
+++ components/Home/Header.js
@@ -3,6 +3,7 @@ import { useUser } from "@supabase/auth-helpers-react";
 
 import TextGradient from "../TextGradient";
 import IconStartGlowing from "../../public/images/star-glowing.svg";
+import IconBadgeCheck from "../../public/images/icons/badge-check.svg";
 import Button from "../Button";
 
 const Header = () => {
@@ -19,6 +20,15 @@ const Header = () => {
             Discover now
           </Button>
         </Link>
+        <div className="h-8"></div>
+        <div className="text-center">
+          <Link passHref href="/the-100-games-promise">
+            <a className="bg-primary-100 rounded-full text-primary-950  text-sm py-1 px-3 inline-flex items-center gap-1 transition-all">
+              <IconBadgeCheck />
+              Do you know the one hundred games promise?
+            </a>
+          </Link>
+        </div>
       </div>
 
       <h2 className="flex items-center justify-center text-2xl font-light mb-4">
